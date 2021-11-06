e0a77b60b6e270f0ec9a8f1046d6d567d7500c54
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-11-06 22:31:01 +0700

    fid by id

--- src/main/java/com/microservice/serviceorder/controllers/CountryController.java
+++ src/main/java/com/microservice/serviceorder/controllers/CountryController.java
@@ -6,6 +6,7 @@ import com.microservice.serviceorder.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -30,5 +31,11 @@ public class CountryController {
         countryRepository.save(param);
 
         return param;
+    }
+
+    @GetMapping("/countries/{id}")
+    @ResponseBody
+    public Country show(@PathVariable int id) {
+        return countryRepository.findById(id).get();
     }
 }
