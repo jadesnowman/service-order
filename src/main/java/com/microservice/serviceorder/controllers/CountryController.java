6e5c8a339a0fef907e7b6327e8227c0fbf5ca201
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-11-06 22:41:58 +0700

    delete by id

--- src/main/java/com/microservice/serviceorder/controllers/CountryController.java
+++ src/main/java/com/microservice/serviceorder/controllers/CountryController.java
@@ -1,10 +1,14 @@
 package com.microservice.serviceorder.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.microservice.serviceorder.models.Country;
 import com.microservice.serviceorder.repositories.CountryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -37,5 +41,18 @@ public class CountryController {
     @ResponseBody
     public Country show(@PathVariable int id) {
         return countryRepository.findById(id).get();
+    }
+
+    @DeleteMapping("/countries/{id}")
+    @ResponseBody
+    public Map<String, String> destroy(@PathVariable int id) {
+
+        countryRepository.deleteById(id);
+
+        Map<String, String> success = new HashMap<String, String>();
+        success.put("success", "true");
+        success.put("message", "Data successfully deleted");
+
+        return success;
     }
 }
